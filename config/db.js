74cb5e77b68e9f7d494cb50b7dc83dc3a28ce25e
Mustafa Manhal <100897710+MustafaManhall@users.noreply.github.com>
2026-08-05 23:57:40 +0300

    fix: db connection

--- config/db.js
+++ config/db.js
@@ -19,6 +19,18 @@ db.connect((err) => {
     console.log('Connected to the database');
 });
 
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000,
+});
+
 // Function to get the user By the email
 const getUserByEmail = async (email) => {
     try {
